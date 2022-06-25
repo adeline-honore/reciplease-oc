9749bc54bfede0df8d29c60c04ff8c4ee9259a6d
adeline-honore <honore_adeline@yahoo.fr>
2022-06-25 03:04:20 +0200

    affichage des éléments dans les cellules de AllRecipesViewController sauf les images

--- Reciplease-oc/AllRecipes/AllRecipesViewController.swift
+++ Reciplease-oc/AllRecipes/AllRecipesViewController.swift
@@ -8,22 +8,60 @@
 import UIKit
 
 class AllRecipesViewController: UIViewController {
-
+    
+    // MARK: - Properties
+    
     var recipes: [Hit]?
+    
+    
+    // MARK: - Outlet
+    
+    @IBOutlet weak var recipesListTableView: UITableView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        recipesListTableView.dataSource = self
+        recipesListTableView.delegate = self
     }
     
-
-    /*
+    
     // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+        
     }
-    */
+    
+}
 
+
+// MARK: - Extension of AllRecipesViewController
+extension AllRecipesViewController: UITableViewDelegate, UITableViewDataSource {
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        guard let recipesCount = recipes?.count else {
+            return 0
+        }
+        return recipesCount
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: RecipesListTableViewCell.identifier, for: indexPath) as? RecipesListTableViewCell else {
+            return UITableViewCell()
+        }
+        guard let recipes = recipes else { return UITableViewCell()}
+        
+        let oneRecipe = recipes[indexPath.row]
+        
+        let imageString = oneRecipe.recipe.image
+        
+        cell.recipesListTitle.text = oneRecipe.recipe.label
+        cell.recipesListTotalTime.text = String(oneRecipe.recipe.totalTime)
+        cell.recipesListIngredientLines.text = oneRecipe.recipe.ingredientLines.joined(separator: " ")
+        
+        return cell
+    }
 }
---
+++ Reciplease-oc/AllRecipes/RecipesListTableViewCell.swift
@@ -0,0 +1,34 @@
+//
+//  RecipesListTableViewCell.swift
+//  Reciplease-oc
+//
+//  Created by HONORE Adeline on 25/06/2022.
+//
+
+import UIKit
+
+class RecipesListTableViewCell: UITableViewCell {
+
+    static let identifier = "recipesListTableViewCell"
+    
+    @IBOutlet weak var recipesListImageView: UIImageView!
+    
+    @IBOutlet weak var recipesListTitle: UILabel!
+    
+    @IBOutlet weak var recipesListTotalTime: UILabel!
+    
+    @IBOutlet weak var recipesListIngredientLines: UILabel!
+    
+    
+    
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+    }
+
+}
